287e841289a6b13de78931d66ab9bee52235f166
Kevin Mahoney <kjmahoney13@gmail.com>
2017-01-22 16:42:47 -0500

    rendered value of all cards

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
-//place three divs within the card
-//place values in those divs
 
-
-
-
-
-
+//declare an array with suits
+var suit = [`♠`, '♥', '♦', '♣']
+//create card and innerHYML for each suit
+var n = -4
 for (i=2;i<15; i++){
+  n = n + 4
+  //while i is 2, make four cards, each with a different suit
+  for (s=0;s<suit.length;s++){
+    //Create a card
+    var card = document.createElement('div');
+    card.className = "card"
+    document.getElementById("table").appendChild(card)
 
-  var card = document.createElement('div');
-  var cardSegmentTop = document.createElement('div');
-  var cardSegmentMiddle = document.createElement('div');
-  var cardSegmentBottom = document.createElement('div');
+    //Add sub-divs to the four cards that were made
+    var cardSegmentTop = document.createElement('div');
+    cardSegmentTop.className = "card-segment-top"
 
+    var cardSegmentMiddle = document.createElement('div');
+    cardSegmentMiddle.className = "card-segment-middle"
 
-  card.className = "card"
-  cardSegmentTop.className = "card-segment-top"
-  cardSegmentMiddle.className = "card-segment-middle"
-  cardSegmentBottom.className = "card-segment-bottom"
+    var cardSegmentBottom = document.createElement('div');
+    cardSegmentBottom.className = "card-segment-bottom"
 
+    console.log(n + s)
+    document.getElementsByClassName("card")[n + s].appendChild(cardSegmentTop)
+    document.getElementsByClassName("card")[n+ s].appendChild(cardSegmentMiddle)
+    document.getElementsByClassName("card")[n + s].appendChild(cardSegmentBottom)
 
-  document.getElementById("table").appendChild(card)
-  var burger = document.getElementsByClassName("card")
-  burger[i-2].appendChild(cardSegmentTop)
-  burger[i-2].appendChild(cardSegmentMiddle)
-  burger[i-2].appendChild(cardSegmentBottom)
-
-  document.getElementsByClassName("card-segment-top")[i-2].innerHTML = `♠`;
-  document.getElementsByClassName("card-segment-middle")[i-2].innerHTML = `♠`;
-  document.getElementsByClassName("card-segment-bottom")[i-2].innerHTML = `♠`;
-
-//use switch case when  time to refactor
-//Print the card number, for cards 11-14, print J,Q,K,A
-  if (i === 11){
-    console.log("J")
+    document.getElementsByClassName("card-segment-top")[n + s].innerHTML = i + suit[s]
+    document.getElementsByClassName("card-segment-middle")[n + s].innerHTML = suit[s]
+    document.getElementsByClassName("card-segment-bottom")[n + s].innerHTML = i + suit[s]
   }
-  else if(i===12){
-    console.log("Q")
-  }
-  else if(i===13){
-    console.log("K")
-  }
-  else if(i===14){
-    console.log("A")
-  }
-  else{
-    console.log(i)
-  }
+
+
+  // if (i === 11){
+  //   console.log("J")
+  // }
+  // else if(i===12){
+  //   console.log("Q")
+  // }
+  // else if(i===13){
+  //   console.log("K")
+  // }
+  // else if(i===14){
+  //   console.log("A")
+  // }
+  // else{
+  //   console.log(i)
+  // }
 
 }
+
+var apple = document.getElementsByClassName("card")
+console.log(apple.length)
