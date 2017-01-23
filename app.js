d753fda6dc0d273e7d11e89e44ed3a9b3b4fb5f3
Kevin Mahoney <kjmahoney13@gmail.com>
2017-01-22 21:35:37 -0500

    add red to diamonds and hearts

--- app.js
+++ app.js
@@ -3,37 +3,47 @@ function deal(){
   //clear cards in case table is already populated
   document.getElementById("table").innerHTML = '';
 
-  //suitArrays to be displayed on table
-  var suitArray = [`♠`, '♥', '♦', '♣']
-  //variable n simplifies iteration through suitArray
+  //suits to be displayed on cards
+  var suitArray = ['♥',`♠`, '♦', '♣']
+
+  //variable n tracks how many cards have been created
   var n = -4
 
-  //iterate through cards 2 to A
+  //iterate through cards 2 through A
   for (i=2; i<15; i++){
-    n = n + 4
-    //For each number 2-A, make four cards, each with a different suit
+
+    //add four to n because each iteration produces 4 cards
+    var n = n + 4
+
+    //For each number 2 through A, make four cards, each with a different suit
     for (s=0;s<suitArray.length;s++){
 
       //Create card
       var card = document.createElement('div');
-      card.className = "card"
+      //diamonds and hearts are red
+      if (s%2==0){
+        card.className = "card red"
+      }else{
+        card.className = "card"
+      }
       document.getElementById("table").appendChild(card)
 
-      //Add top, middle, and bottom divs to each card for styling purposes
+      //Add top, middle, and bottom divs to each card in order to display card value
       var cardSegmentTop = document.createElement('div');
       cardSegmentTop.className = "card-segment top"
+      document.getElementsByClassName("card")[n + s].appendChild(cardSegmentTop)
+
 
       var cardSegmentMiddle = document.createElement('div');
       cardSegmentMiddle.className = "card-segment middle"
+      document.getElementsByClassName("card")[n + s].appendChild(cardSegmentMiddle)
+
 
       var cardSegmentBottom = document.createElement('div');
       cardSegmentBottom.className = "card-segment bottom"
-
-      document.getElementsByClassName("card")[n + s].appendChild(cardSegmentTop)
-      document.getElementsByClassName("card")[n + s].appendChild(cardSegmentMiddle)
       document.getElementsByClassName("card")[n + s].appendChild(cardSegmentBottom)
 
-      //give face cards a letter instead of number
+      //face cards display a letter instead of a number
       if (i === 11){
         var number = "J"
       }
@@ -50,15 +60,7 @@ function deal(){
         var number = i
       }
 
-      //place symbol and number in top and bottom of card
-      var suitDisplay = document.createElement('p');
-      suitDisplay.className = "suit-display"
-      document.getElementsByClassName("top")[n + s].appendChild(suitDisplay).innerHTML = number + suitArray[s]
-      var suitDisplay = document.createElement('p');
-      suitDisplay.className = "suit-display"
-      document.getElementsByClassName("bottom")[n + s].appendChild(suitDisplay).innerHTML = number + suitArray[s]
-
-      //unless a face card, middle of card displays a suit symbol based on number value
+      //place symbols on middle of card, based on card value. Face cards get one large symbol
       if (i >= 11){
         var suitDisplay = document.createElement('p');
         suitDisplay.className = "suit-display big"
@@ -70,11 +72,15 @@ function deal(){
           document.getElementsByClassName("middle")[n + s].appendChild(suitDisplay).innerHTML = suitArray[s]
         }
       }
-
+      //place symbol and number on top and bottom of card
+      var suitDisplay = document.createElement('p');
+      suitDisplay.className = "suit-display"
+      document.getElementsByClassName("top")[n + s].appendChild(suitDisplay).innerHTML = number + suitArray[s]
+      var suitDisplay = document.createElement('p');
+      suitDisplay.className = "suit-display"
+      document.getElementsByClassName("bottom")[n + s].appendChild(suitDisplay).innerHTML = number + suitArray[s]
     }
-
   }
-
 }
 
 //clear cards from table
